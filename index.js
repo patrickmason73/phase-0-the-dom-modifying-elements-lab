0c4b4d31779e188d54b6b7c3795508d2fb2e85d8
Patrick Mason <patrickmason73@gmail.com>
2022-08-08 18:20:05 -0600

    commit tree 2aaf8b663a904cf7d1e25d51ed84f3f88a66687d

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// Write your code here!
+main.remove()
+
+const element = document.createElement('h1')
+document.body.appendChild(element)
+element.setAttribute('id', 'victory')
+const newHeader = document.querySelector('h1#victory')
+element.innerHTML = "YOUR-NAME is the champion"
